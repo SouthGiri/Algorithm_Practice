2506ba71e79e2a8d12e629649bcf5be4a9785965
Lee Namgil <114569746+SouthGiri@users.noreply.github.com>
2024-07-07 23:42:43 +0900

    [Bronze IV] Title: 방학 숙제, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+from math import ceil
+
+l = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(l - max(ceil(a/c), ceil(b/d)))
