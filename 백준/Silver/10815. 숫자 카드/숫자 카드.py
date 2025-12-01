a77313041d7dff99ab32a5723375bf1ecdb4a35b
Lee Namgil <114569746+SouthGiri@users.noreply.github.com>
2025-12-01 21:03:49 +0900

    [Silver V] Title: 숫자 카드, Time: 592 ms, Memory: 118300 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,26 +1,8 @@
-def bin_sear(data, target):
-    start = 0
-    end = len(data)-1
-    while start <= end:
-        mid = (start+end) // 2
-        if data[mid] == target:
-            return True
-        if data[mid] < target:
-            start = mid+1
-        else:
-            end = mid-1
-    return False
-
-    
 N = int(input())
-data = list(map(int, input().split()))
+data = set(map(int, input().split()))
 
 M = int(input())
 cards = list(map(int, input().split()))
 
-data.sort()
 for card in cards:
-    if bin_sear(data, card):
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+    print(1, end=' ') if card in data else print(0, end=' ')
