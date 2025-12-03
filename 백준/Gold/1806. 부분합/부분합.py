6fcb01f0efe37c2a2cb0c289cfd706697de6354c
Lee Namgil <114569746+SouthGiri@users.noreply.github.com>
2025-12-03 16:27:49 +0900

    [Gold IV] Title: 부분합, Time: 104 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -2,21 +2,19 @@ INF = int(1e9)
 N, S = map(int, input().split())
 arr = list(map(int, input().split()))
 i = j = 0
-sum_val = arr[0]
+sum_val = 0
 ans = INF
 
-while i < N and j < N:
-    if i > j:
-        j = i
-    while sum_val < S and j < N-1:
-        j += 1
-        sum_val += arr[j]
-        
+while True:
     if sum_val >= S:
-        ans = min(ans, j-i+1)
-    
-    sum_val -= arr[i]
-    i += 1
+        ans = min(ans, j-i)
+        sum_val -= arr[i]
+        i += 1
+    elif j == N:
+        break
+    else:
+        sum_val += arr[j]
+        j += 1
 
 if ans == INF:
     print(0)
