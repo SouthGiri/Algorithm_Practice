cd6c6059b276d493cf9b2500342f815556c304c3
Lee Namgil <114569746+SouthGiri@users.noreply.github.com>
2026-01-08 09:53:18 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,16 @@
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE
+        WHEN status = 'SALE' THEN '판매중'
+        WHEN status = 'RESERVED' THEN '예약중'
+        WHEN status = 'DONE' THEN '거래완료'
+    END AS STATUS
+FROM
+    used_goods_board
+WHERE
+    created_date = '2022-10-05'
+ORDER BY
+    board_id DESC
